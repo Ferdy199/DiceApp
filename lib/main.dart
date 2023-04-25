5a1e7ee337790e909a154693e2a2717a76194836
Ferdy199 <fdumbadumba@gmail.com>
2023-04-26 01:11:26 +0700

    changes the color alignment (Chap 1 : Step 31)

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,11 @@ void main() {
     home: Scaffold(
       body: Container(
         decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(244, 216, 148, 46),
-              Color.fromARGB(255, 230, 216, 97),
-              Colors.white
-            ],
-          ),
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(244, 216, 148, 46),
+            Color.fromARGB(255, 230, 216, 97),
+            Color.fromARGB(255, 238, 231, 166)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
         ),
         child: const Center(
           child: Text(
